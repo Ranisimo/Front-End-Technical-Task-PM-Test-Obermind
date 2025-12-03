d9a4d68839da79215e70a67e80d92dcedc8f4485
kizzysinar <kizzysinar@gmail.com>
2025-12-03 13:54:38 +0000

    first two sections of homepage

---
+++ my-app/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <header className="w-full p-4 bg-gray-200 dark:bg-gray-800">
+      <h1 className="text-2xl font-bold text-center text-gray-900 dark:text-gray-100">
+        My Application Footer
+      </h1>
+    </header>
+  );
+}
